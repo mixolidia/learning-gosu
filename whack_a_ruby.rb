0140bc81172acd6e7f73aab896dbc37b70eef728
mixolidia <mixolidia@gmail.com>
2016-09-22 03:11:21 -0400

    Add instance of Whack a Ruby for testing Add show method to test Edit capitalize R in window caption

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -4,6 +4,9 @@ class WhackARuby < Gosu::Window
 
   def initialize
     super(800, 600)
-    self.caption = "Whack it good! Get that ruby!"
+    self.caption = "Whack it good! Get that Ruby!"
   end
 end
+
+window = WhackARuby.new
+window.show
