7aa0eccef23d7c73998b598df68cd9433ad85593
mixolidia <mixolidia@gmail.com>
2016-09-23 02:46:13 -0400

    Add images folder Add ruby and hammer images to images folder Create image instance to add ruby image to game Fix spaces and line in whack_a_ruby.rb

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -1,10 +1,11 @@
 require 'gosu'
 
+# this class opens game window
 class WhackARuby < Gosu::Window
-
   def initialize
     super(800, 600)
-    self.caption = "Whack it good! Get that Ruby!"
+    self.caption = 'Whack it good! Get that Ruby!'
+    @image = Gosu::Image.new('ruby.png')
   end
 end
 
