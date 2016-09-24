4e5c7f8bbf8379164341281f51de9956129b29d6
mixolidia <mixolidia@gmail.com>
2016-09-24 02:05:30 -0400

    Add placement of ruby image as x and y coordinates in initialize method

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -6,10 +6,12 @@ class WhackARuby < Gosu::Window
     super(800, 600)
     self.caption = 'Whack it good! Get that Ruby!'
     @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
   end
 
   def draw
-    
+
   end
 end
 
